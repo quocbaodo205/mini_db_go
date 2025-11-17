eb868381f9bff09b3e2d3d1879c5bbdfec16989e
quoc.do <quoc.do@bytedance.com>
2025-11-17 23:05:36 +0800

    Add leaf node and insert function + test

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const INTERNAL_MAX_KEY = 4
 
-type Node interface{}
+type Node any
 
 type BTreeInternalNode struct {
 	nkey     int
@@ -70,6 +70,157 @@ func (node *BTreeInternalNode) Split() BTreeInternalNode {
 	}
 	node.nkey = pos
 	return newNode
+}
+
+// Define leaf node
+type BTreeLeafNode struct {
+	nkey   int
+	keys   [INTERNAL_MAX_KEY]int
+	values [INTERNAL_MAX_KEY]int
+}
+
+func NewLNode() BTreeLeafNode {
+	var new_keys [INTERNAL_MAX_KEY]int
+	var new_vals [INTERNAL_MAX_KEY]int
+	return BTreeLeafNode{
+		nkey:   0,
+		keys:   new_keys,
+		values: new_vals,
+	}
+}
+
+// Find last position so that the key <= find_key
+func (node *BTreeLeafNode) FindLastLE(findKey int) int {
+	pos := -1
+	for i := 0; i < node.nkey; i++ {
+		if node.keys[i] <= findKey {
+			pos = i
+		}
+	}
+	return pos
+}
+
+// Insert a key-children pair into the Internal Node
+func (node *BTreeLeafNode) InsertKV(insertKey int, insertValue int) {
+	// Find last less or equal as position to insert
+	pos := node.FindLastLE(insertKey)
+	// [ 1,4,7,| | ] -> insert 3
+	// [ 1,| |,4,7 ] -> insert 3
+	for i := node.nkey - 1; i > pos; i-- {
+		node.keys[i+1] = node.keys[i]
+		node.values[i+1] = node.values[i]
+	}
+	node.keys[pos+1] = insertKey
+	node.values[pos+1] = insertValue
+	// [ 1,3,4,7 ]
+	node.nkey += 1
+}
+
+// Split a node into 2 equal part
+func (node *BTreeLeafNode) Split() BTreeLeafNode {
+	var newKeys [INTERNAL_MAX_KEY]int
+	var newValues [INTERNAL_MAX_KEY]int
+	// Split in the middle
+	pos := node.nkey / 2
+	// [ 1 , 2 , 0 , 0 ] -> pos = 2
+	// [ 3 , 4 , 0 , 0 ]
+	for i := pos; i < node.nkey; i++ {
+		newKeys[i-pos] = node.keys[i] // n[0] = o[2]
+		newValues[i-pos] = node.values[i]
+		node.keys[i] = 0
+		node.values[i] = 0
+	}
+	newNode := BTreeLeafNode{
+		nkey:   node.nkey - pos,
+		keys:   newKeys,
+		values: newValues,
+	}
+	node.nkey = pos
+	return newNode
+}
+
+// B+Tree structure
+type BPTree struct {
+	head Node
+}
+
+func NewBPTree() BPTree {
+	newINode := NewINode()
+	return BPTree{
+		head: &newINode,
+	}
+}
+
+func (tree *BPTree) insertRecursive(node Node, insertKey int, insertValue int) Node {
+	// Insert a key value pair.
+	// Current: [3] | 3 -> [(3,3), (5,5)]
+	if convert, ok := node.(*BTreeInternalNode); ok {
+		pos := convert.FindLastLE(insertKey) // -> -1
+		if convert.nkey == 0 {
+			// Insert in the begining
+			firstLeaf := NewLNode()
+			firstLeaf.InsertKV(insertKey, insertValue)
+			convert.InsertKV(insertKey, &firstLeaf)
+		} else {
+			// Special process for -1 position
+			if pos == -1 {
+				pos = 0
+			}
+			child := convert.children[pos]
+			// child -> [(2,2), (3,3), (5,5)]
+			// Current: [3] -> [(2,2), (3,3), (5,5)]
+			// Node -> any (*BTreeInternalNode / *BTreeLeafNode)
+			// Child *Node -> Node
+			insertResult := tree.insertRecursive(*child, insertKey, insertValue)
+			// Take the first key of children and put as promotion key
+			if childConvert, ok := (*child).(*BTreeLeafNode); ok {
+				convert.keys[pos] = childConvert.keys[0]
+			} else {
+				childConvert := (*child).(*BTreeInternalNode)
+				convert.keys[pos] = childConvert.keys[0]
+			}
+			// Current: [2] -> [(2,2), (3,3), (5,5)]
+			// If need split, insert back to parent.
+			if insertResult != nil {
+				if childConvert, ok := insertResult.(*BTreeLeafNode); ok {
+					convert.InsertKV(childConvert.keys[0], childConvert)
+				} else {
+					childConvert := insertResult.(*BTreeInternalNode)
+					convert.InsertKV(childConvert.keys[0], childConvert)
+				}
+			}
+			// After insert, check if need split.
+			if convert.nkey == INTERNAL_MAX_KEY {
+				newInternal := convert.Split()
+				return &newInternal
+			}
+		}
+	} else {
+		convert := node.(*BTreeLeafNode)
+		convert.InsertKV(insertKey, insertValue)
+
+		// After insert, check if need split.
+		if convert.nkey == INTERNAL_MAX_KEY {
+			newLeaf := convert.Split()
+			return &newLeaf
+		}
+	}
+
+	return nil
+}
+
+func (tree *BPTree) Insert(insertKey int, insertValue int) {
+	insertResult := tree.insertRecursive(tree.head, insertKey, insertValue)
+	if insertResult != nil {
+		childConvert := insertResult.(*BTreeInternalNode)
+		newHead := NewINode()
+		newHead.nkey = 2
+		newHead.keys[0] = tree.head.(*BTreeInternalNode).keys[0]
+		newHead.keys[1] = childConvert.keys[0]
+		newHead.children[0] = tree.head.(*BTreeInternalNode).children[0]
+		newHead.children[1] = childConvert.children[0]
+		tree.head = &newHead
+	}
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestDatabase(t *testing.T) {
+func TestINode(t *testing.T) {
 	node := NewINode()
 	c := NewINode()
 	node.InsertKV(3, c)
@@ -65,6 +65,158 @@ func TestDatabase(t *testing.T) {
 	if newNode.keys[1] != 12 {
 		t.Errorf("got key[2] = %v, expect %v", newNode.keys[1], 10)
 	}
-	// if d, ok := (*node.children[0]).(BTreeInternalNode); ok {
-	// }
+}
+
+func TestLNode(t *testing.T) {
+	node := NewLNode()
+	node.InsertKV(3, 1)
+	// [3]
+	if node.nkey != 1 {
+		t.Errorf("got nkey = %v, expect %v", node.nkey, 1)
+	}
+	if node.keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", node.keys[0], 3)
+	}
+	node.InsertKV(10, 1)
+	// [3, 10]
+	if node.nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", node.nkey, 2)
+	}
+	if node.keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", node.keys[0], 3)
+	}
+	if node.keys[1] != 10 {
+		t.Errorf("got key[1] = %v, expect %v", node.keys[1], 10)
+	}
+	node.InsertKV(5, 1)
+	// [3, 5, 10]
+	if node.nkey != 3 {
+		t.Errorf("got nkey = %v, expect %v", node.nkey, 3)
+	}
+	if node.keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", node.keys[0], 3)
+	}
+	if node.keys[1] != 5 {
+		t.Errorf("got key[1] = %v, expect %v", node.keys[1], 5)
+	}
+	if node.keys[2] != 10 {
+		t.Errorf("got key[2] = %v, expect %v", node.keys[2], 10)
+	}
+	node.InsertKV(12, 1)
+	if node.nkey != 4 {
+		t.Errorf("got nkey = %v, expect %v", node.nkey, 4)
+	}
+	// [3, 5, 10, 12]
+	newNode := node.Split()
+	// [3, 5] [10, 12]
+	if node.nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", node.nkey, 2)
+	}
+	if newNode.nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", newNode.nkey, 2)
+	}
+	if node.keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", node.keys[0], 3)
+	}
+	if node.keys[1] != 5 {
+		t.Errorf("got key[1] = %v, expect %v", node.keys[1], 5)
+	}
+	if newNode.keys[0] != 10 {
+		t.Errorf("got key[2] = %v, expect %v", newNode.keys[0], 10)
+	}
+	if newNode.keys[1] != 12 {
+		t.Errorf("got key[2] = %v, expect %v", newNode.keys[1], 10)
+	}
+}
+
+func TestBTree(t *testing.T) {
+	tree := NewBPTree()
+	// head = inode[]
+	tree.Insert(3, 3)
+	// head = inode[3] , 3 -> lnode[(3,3)]
+	if tree.head.(*BTreeInternalNode).keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", tree.head.(BTreeInternalNode).keys[0], 3)
+	}
+	child := tree.head.(*BTreeInternalNode).children[0]
+	if (*child).(*BTreeLeafNode).keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", (*child).(*BTreeLeafNode).keys[0], 3)
+	}
+	if (*child).(*BTreeLeafNode).values[0] != 3 {
+		t.Errorf("got values[0] = %v, expect %v", (*child).(*BTreeLeafNode).values[0], 3)
+	}
+
+	// head = inode[3] , 3 -> lnode[(3,3)]
+	tree.Insert(5, 5)
+	// head = inode[3] , 3 -> lnode[(3,3), (5,5)]
+	if tree.head.(*BTreeInternalNode).keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", tree.head.(BTreeInternalNode).keys[0], 3)
+	}
+	child = tree.head.(*BTreeInternalNode).children[0]
+	if (*child).(*BTreeLeafNode).nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", (*child).(*BTreeLeafNode).nkey, 2)
+
+	}
+	if (*child).(*BTreeLeafNode).keys[0] != 3 {
+		t.Errorf("got key[0] = %v, expect %v", (*child).(*BTreeLeafNode).keys[0], 3)
+	}
+	if (*child).(*BTreeLeafNode).values[0] != 3 {
+		t.Errorf("got values[0] = %v, expect %v", (*child).(*BTreeLeafNode).values[0], 3)
+	}
+	if (*child).(*BTreeLeafNode).keys[1] != 5 {
+		t.Errorf("got key[1] = %v, expect %v", (*child).(*BTreeLeafNode).keys[1], 5)
+	}
+	if (*child).(*BTreeLeafNode).values[1] != 5 {
+		t.Errorf("got values[1] = %v, expect %v", (*child).(*BTreeLeafNode).values[1], 5)
+	}
+
+	// head = inode[3] , 3 -> lnode[(3,3), (5,5)]
+	tree.Insert(2, 2)
+	// head = inode[2] , 2 -> lnode[(2,2), (3,3), (5,5)]
+	child = tree.head.(*BTreeInternalNode).children[0]
+	if (*child).(*BTreeLeafNode).nkey != 3 {
+		t.Errorf("got nkey = %v, expect %v", (*child).(*BTreeLeafNode).nkey, 3)
+	}
+	if (*child).(*BTreeLeafNode).keys[0] != 2 {
+		t.Errorf("got key[0] = %v, expect %v", (*child).(*BTreeLeafNode).keys[0], 2)
+	}
+	if (*child).(*BTreeLeafNode).keys[1] != 3 {
+		t.Errorf("got key[1] = %v, expect %v", (*child).(*BTreeLeafNode).keys[1], 3)
+	}
+	if (*child).(*BTreeLeafNode).keys[2] != 5 {
+		t.Errorf("got key[2] = %v, expect %v", (*child).(*BTreeLeafNode).keys[2], 5)
+	}
+
+	// head = inode[2] , 2 -> lnode[(2,2), (3,3), (5,5)]
+	tree.Insert(8, 8)
+	// head = inode[2] , 2 -> lnode[(2,2), (3,3), (5,5), (8,8)]
+	// head = inode[2,5], 2 -> lnode[(2,2),(3,3)], 5 -> lnode[(5,5),(8,8)]
+	if tree.head.(*BTreeInternalNode).nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", tree.head.(*BTreeInternalNode).nkey, 2)
+	}
+	if tree.head.(*BTreeInternalNode).keys[0] != 2 {
+		t.Errorf("got keys[0] = %v, expect %v", tree.head.(*BTreeInternalNode).keys[0], 2)
+	}
+	if tree.head.(*BTreeInternalNode).keys[1] != 5 {
+		t.Errorf("got keys[0] = %v, expect %v", tree.head.(*BTreeInternalNode).keys[1], 5)
+	}
+	child = tree.head.(*BTreeInternalNode).children[1]
+	// lnode[(5,5),(8,8)]
+	if (*child).(*BTreeLeafNode).nkey != 2 {
+		t.Errorf("got nkey = %v, expect %v", (*child).(*BTreeLeafNode).nkey, 2)
+	}
+	if (*child).(*BTreeLeafNode).keys[0] != 5 {
+		t.Errorf("got key[0] = %v, expect %v", (*child).(*BTreeLeafNode).keys[0], 5)
+	}
+	if (*child).(*BTreeLeafNode).keys[1] != 8 {
+		t.Errorf("got key[1] = %v, expect %v", (*child).(*BTreeLeafNode).keys[1], 8)
+	}
+}
+
+func TestBTreeBig(t *testing.T) {
+	// Renew test
+	tree := NewBPTree()
+	for i := range 100 {
+		tree.Insert(i, i)
+	}
+
 }
